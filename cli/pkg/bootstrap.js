d8b52e8a7633575d873be43af2b0fc31db03b4bd
devnet0x <devnet0x@gmail.com>
2024-01-04 16:56:50 -0300

    Wasm prover first commit

--- cli/pkg/bootstrap.js
+++ cli/pkg/bootstrap.js
@@ -2,14 +2,16 @@ import init, { main2 } from './sandstorm_cli.js';
 
 async function run() {
     await init();
-
-    document.getElementById('calculateButton').addEventListener('click', async () => {
+    /*****************
+     * verify button *
+     *****************/
+    document.getElementById('verifyButton').addEventListener('click', async () => {
         let program_json = '';
         let public_input_json = '';
         let proof = '';
-
+        
         try {
-            const programResponse = await fetch('./array-sum.json');
+            const programResponse = await fetch("array-sum.json") // await fetch(document.getElementById('program').files[0].name);
             if (programResponse.ok) {
                 program_json = await programResponse.text();
             }
@@ -18,7 +20,7 @@ async function run() {
         }
 
         try {
-            const publicInputResponse = await fetch('./air-public-input.json');
+            const publicInputResponse = await fetch("air-public-input.json") //await fetch(document.getElementById('publicInput').files[0].name);
             if (publicInputResponse.ok) {
                 public_input_json = await publicInputResponse.text();
             }
@@ -27,7 +29,7 @@ async function run() {
         }
 
         try {
-            const proofResponse = await fetch('./array-sum.proof');
+            const proofResponse = await fetch("array-sum.proof") //await fetch(document.getElementById('proof').files[0].name);
             if (proofResponse.ok) {
                 const arrayBuffer = await proofResponse.arrayBuffer();
                 const uint8Array = new Uint8Array(arrayBuffer);
@@ -37,23 +39,94 @@ async function run() {
             console.error('Error fetching proof file:', error);
         }
 
-        //console.log('Program JSON:', program_json);
-        //console.log('Public input JSON:', public_input_json);
+        const start = performance.now();
         try {
-            // get now previous to main2 and elapsed time after main2 call
-            const start = performance.now();
-            main2(program_json, public_input_json, proof);
+            let result = main2("Verify", program_json, public_input_json, proof, program_json, program_json, program_json);
             const end = performance.now();
             const elapsed = end - start;
+            console.log("result: ", result);
 
             document.getElementById('result').innerText = "Verification successful! (elapsed time: " + elapsed + " ms)";
         } catch (error) {
             const end = performance.now();
             const elapsed = end - start;
-            document.getElementById('result').innerText = "Verification failed!(elapsed time: " + elapsed + " ms)";
+            document.getElementById('result').innerText = "Verification failed!(elapsed time: " + elapsed + " ms)" + error;
         }
+    });
+
+    /*****************
+     * proof button  *
+     *****************/
+    document.getElementById('proofButton').addEventListener('click', async () => {
+        let program_json = '';
+        let public_input_json = '';
+        let private_input_json = '';
+        let trace = '';
+        let memory = '';
         
-        
+        try {
+            const programResponse = await fetch("array-sum.json") // await fetch(document.getElementById('program1').files[0].name);
+            if (programResponse.ok) {
+                program_json = await programResponse.text();
+            }
+        } catch (error) {
+            console.error('Error fetching program JSON:', error);
+        }
+
+        try {
+            const publicInputResponse = await fetch("air-public-input.json") //await fetch(document.getElementById('publicInput1').files[0].name);
+            if (publicInputResponse.ok) {
+                public_input_json = await publicInputResponse.text();
+            }
+        } catch (error) {
+            console.error('Error fetching public input JSON:', error);
+        }
+
+        try {
+            const privateInputResponse = await fetch("air-private-input.json") //await fetch(document.getElementById('publicInput1').files[0].name);
+            if (privateInputResponse.ok) {
+                private_input_json = await privateInputResponse.text();
+            }
+        } catch (error) {
+            console.error('Error fetching public input JSON:', error);
+        }
+
+        try {
+            const traceResponse = await fetch("trace.bin") //await fetch(document.getElementById('trace1').files[0].name);
+            if (traceResponse.ok) {
+                const arrayBuffer = await traceResponse.arrayBuffer();
+                const uint8Array = new Uint8Array(arrayBuffer);
+                trace = uint8Array;
+            }
+        } catch (error) {
+            console.error('Error fetching trace file:', error);
+        }
+
+        try {
+            const memoryResponse = await fetch("memory.bin") //await fetch(document.getElementById('memory1').files[0].name);
+            if (memoryResponse.ok) {
+                const arrayBuffer = await memoryResponse.arrayBuffer();
+                const uint8Array = new Uint8Array(arrayBuffer);
+                memory = uint8Array;
+            }
+        } catch (error) {
+            console.error('Error fetching memory file:', error);
+        }
+
+        const start = performance.now();
+        try {
+            let dummy = trace;
+            let result = main2("Prove", program_json, public_input_json, dummy, trace, memory, private_input_json);
+            const end = performance.now();
+            const elapsed = end - start;
+            console.log("result: ", result);
+
+            document.getElementById('result1').innerText = "Verification successful! (elapsed time: " + elapsed + " ms)";
+        } catch (error) {
+            const end = performance.now();
+            const elapsed = end - start;
+            document.getElementById('result1').innerText = "Verification failed!(elapsed time: " + elapsed + " ms)" + error;
+        }
     });
 }
 
--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -40,10 +40,6 @@ const GOLDILOCKS_PRIME_HEX_STR: &str = "0xffffffff00000001";
 #[derive(StructOpt, Debug)]
 enum Command {
     Prove {
-        #[structopt(long, parse(from_os_str))]
-        output: PathBuf,
-        #[structopt(long, parse(from_os_str))]
-        air_private_input: PathBuf,
         // TODO: add validation to the proof options
         #[structopt(long, default_value = "65")]
         num_queries: u8,
@@ -67,23 +63,43 @@ fn convert_string_to_value(json_str: &str) -> Result<Value, serde_json::Error> {
     Ok(value)
 }
 
+// Send  panic messages to the console.error
 #[wasm_bindgen(start)]
 pub fn start() {
     console_error_panic_hook::set_once();
 }
 
 #[wasm_bindgen]
-pub fn main2(program_json_str: String,air_public_input_json_str: String,binary_data: &[u8]) {
+pub fn main2(command: String, 
+    program_json_str: String,
+    air_public_input_json_str: String,
+    proof_file: &[u8], 
+    trace_file: &[u8],
+    memory_file: &[u8],
+    air_private_input_json_str: String) -> Vec<u8> {
     //log_bytes(binary_data);
-    let proof: &[u8] = binary_data;
-    let command: Command = Command::Verify {
-        required_security_bits: 80
+    // Parse the command
+    //web_sys::console::log_1(&JsValue::from_str(line!().to_string().as_str()));
+    web_sys::console::log_1(&JsValue::from_str("010"));
+    let output_str = "".to_string();
+    let command: Command = match command.as_str() {
+        "Verify" => Command::Verify { required_security_bits: 80 },
+        "Prove" => Command::Prove {
+            num_queries: 65,
+            lde_blowup_factor: 2,
+            proof_of_work_bits: 16,
+            fri_folding_factor: 8,
+            fri_max_remainder_coeffs: 16,
+        },
+        _ => panic!("Unknown command"),
     };
+    web_sys::console::log_1(&JsValue::from_str("020"));
     let air_public_input_json: JsValue = JsValue::from_str(&air_public_input_json_str);
-
+    web_sys::console::log_1(&JsValue::from_str("030"));
     let data_str: String = air_public_input_json.as_string().unwrap();
     let data_reader = Cursor::new(data_str);
-
+    web_sys::console::log_1(&JsValue::from_str("040"));
+    // TODO: Read prime field from program_json_str json string
     let prime: String = "0x800000000000011000000000000000000000000000000000000000000000001".to_string();
     match prime.to_lowercase().as_str() {
         STARKWARE_PRIME_HEX_STR => {
@@ -95,12 +111,14 @@ pub fn main2(program_json_str: String,air_public_input_json_str: String,binary_d
                 Layout::Starknet => {
                     use claims::starknet::EthVerifierClaim;
                     let claim = EthVerifierClaim::new(program, air_public_input);
-                    execute_command(command, claim, proof);
+                    execute_command(command, claim, proof_file, trace_file, memory_file, air_private_input_json_str, output_str)
                 }
                 Layout::Recursive => {
                     use claims::recursive::CairoVerifierClaim;
+                    web_sys::console::log_1(&JsValue::from_str("050"));
                     let claim = CairoVerifierClaim::new(program, air_public_input);
-                    execute_command(command, claim, proof);
+                    web_sys::console::log_1(&JsValue::from_str("060"));
+                    execute_command(command, claim, proof_file, trace_file, memory_file, air_private_input_json_str, output_str)
                 }
                 _ => unimplemented!(),
             }
@@ -143,12 +161,14 @@ pub fn main2(program_json_str: String,air_public_input_json_str: String,binary_d
 fn execute_command<Fp: PrimeField, Claim: Stark<Fp = Fp, Witness = CairoWitness<Fp>>>(
     command: Command,
     claim: Claim,
-    proof: &[u8],
-) {
+    proof_file: &[u8],
+    trace_file: &[u8],
+    memory_file: &[u8],
+    air_private_input: String,
+    output: String,
+) -> Vec<u8> {
     match command {
         Command::Prove {
-            output,
-            air_private_input,
             num_queries,
             lde_blowup_factor,
             proof_of_work_bits,
@@ -162,11 +182,12 @@ fn execute_command<Fp: PrimeField, Claim: Stark<Fp = Fp, Witness = CairoWitness<
                 fri_folding_factor,
                 fri_max_remainder_coeffs,
             );
-            prove(options, &air_private_input, &output, claim)
+            web_sys::console::log_1(&JsValue::from_str("070"));
+            prove(options, air_private_input, &trace_file, &memory_file, &output, claim)
         }
         Command::Verify {
             required_security_bits,
-        } => verify(required_security_bits, &proof, claim),
+        } => verify(required_security_bits, &proof_file, claim),
     }
 }
 
@@ -177,10 +198,9 @@ pub fn log_bytes(bytes: &[u8]) {
 
 fn verify<Claim: Stark<Fp = impl Field>>(
     required_security_bits: u8,
-    proof_path: &[u8],
+    proof_bytes: &[u8],
     claim: Claim,
-) {
-    let proof_bytes = proof_path;
+) -> Vec<u8> {
     //log_bytes(proof_bytes);
     let proof = Proof::<Claim>::deserialize_compressed(&*proof_bytes).unwrap();
     //let now = window().unwrap().performance().unwrap().now();    
@@ -188,39 +208,52 @@ fn verify<Claim: Stark<Fp = impl Field>>(
     //let elapsed = window().unwrap().performance().unwrap().now() - now;
 
     //println!("Proof verified in: {:?}", elapsed);
+    0u8.to_be_bytes().to_vec()
 }
 
 fn prove<Fp: PrimeField, Claim: Stark<Fp = Fp, Witness = CairoWitness<Fp>>>(
     options: ProofOptions,
-    private_input_path: &PathBuf,
-    output_path: &PathBuf,
+    private_input_file: String,
+    trace_file: &[u8],
+    memory_file: &[u8],
+    output_path: &String,
     claim: Claim,
-) {
-    let private_input_file =
-        File::open(private_input_path).expect("could not open private input file");
-    let private_input: AirPrivateInput = serde_json::from_reader(private_input_file).unwrap();
+) -> Vec<u8> {
+    //let private_input_file =
+    //    File::open(private_input_path).expect("could not open private input file");
+    web_sys::console::log_1(&JsValue::from_str("080"));
+    let private_input: AirPrivateInput = serde_json::from_reader(private_input_file.as_bytes()).unwrap();
 
-    let trace_path = &private_input.trace_path;
-    let trace_file = File::open(trace_path).expect("could not open trace file");
+    //let trace_path = &private_input.trace_path;
+    //let trace_file = File::open(trace_path).expect("could not open trace file");
+    web_sys::console::log_1(&JsValue::from_str("090"));
     let register_states = RegisterStates::from_reader(trace_file);
 
-    let memory_path = &private_input.memory_path;
-    let memory_file = File::open(memory_path).expect("could not open memory file");
+    //let memory_path = &private_input.memory_path;
+    //let memory_file = File::open(memory_path).expect("could not open memory file");
+    web_sys::console::log_1(&JsValue::from_str("100"));
     let memory = Memory::from_reader(memory_file);
 
+    web_sys::console::log_1(&JsValue::from_str("110"));
     let witness = CairoWitness::new(private_input, register_states, memory);
 
-    let now = Instant::now();
+    //let now = Instant::now();
+    web_sys::console::log_1(&JsValue::from_str("120"));
     let proof = pollster::block_on(claim.prove(options, witness)).unwrap();
-    println!("Proof generated in: {:?}", now.elapsed());
+    //println!("Proof generated in: {:?}", now.elapsed());
+    web_sys::console::log_1(&JsValue::from_str("130"));
     let security_level_bits = proof.security_level_bits();
-    println!("Proof security (conjectured): {security_level_bits}bit");
+    //println!("Proof security (conjectured): {security_level_bits}bit");
 
+    web_sys::console::log_1(&JsValue::from_str("140"));
     let mut proof_bytes = Vec::new();
+    web_sys::console::log_1(&JsValue::from_str("150"));
     proof.serialize_compressed(&mut proof_bytes).unwrap();
-    println!("Proof size: {:?}KB", proof_bytes.len() / 1024);
-    let mut f = File::create(output_path).unwrap();
-    f.write_all(proof_bytes.as_slice()).unwrap();
-    f.flush().unwrap();
-    println!("Proof written to {}", output_path.as_path().display());
+    web_sys::console::log_1(&JsValue::from_str("160"));
+    proof_bytes
+    // println!("Proof size: {:?}KB", proof_bytes.len() / 1024);
+    // let mut f = File::create(output_path).unwrap();
+    // f.write_all(proof_bytes.as_slice()).unwrap();
+    // f.flush().unwrap();
+    // println!("Proof written to {}", output_path.as_path().display());
 }
